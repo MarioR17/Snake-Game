2632dbdc63ee7de7e4a0a7705bfc7898a6766df0
Oni Ahmetaj <onia@Onis-MacBook-Air.local>
2023-05-22 15:46:39 -0400

    Screen Display

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
-print("Hello World")
+import pygame
+from sys import exit
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Snake")
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("purple")
+
+    pygame.display.flip()
+
+    clock.tick(60)
+pygame.quit()
+exit()
