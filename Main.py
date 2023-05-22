f47d3c1b2af4ac9e9b716019e4b6ead8784277fb
Mario Rendon <mariorendon@Marios-MacBook-Air-2.local>
2023-05-22 15:44:16 -0400

    Initialized Screen

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
-print("Hello World")
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Snake Game")
+
+clock = pygame.time.Clock()
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.update()
+    clock.tick(60)
+
+
