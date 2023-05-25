f782436f8652945c8eb923f32f23a4d0b63dbc52
Mario Rendon <mariorendon@Marios-MacBook-Air-2.local>
2023-05-25 16:42:02 -0400

    Different checkerboard and partly moving snake head

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ score = 0;
 HighScore = 0;
 screen = pygame.display.set_mode((650, 650))
 pygame.display.set_caption("Snake Game")
-game_surface = pygame.image.load("BackGrounds/CheckerBoard.png").convert()
+game_surface = pygame.image.load("BackGrounds/Checker.png").convert()
 Font = pygame.font.SysFont("comicsansms", 30)
 Outer1_Surface = pygame.Surface((650, 100))
 Outer2_Surface = pygame.Surface((650, 650))
@@ -18,16 +18,18 @@ clock = pygame.time.Clock()
 Font_surface = Font.render(f"Score: {score}", True, "black")
 Highscore_surface = Font.render(f"High Score: {HighScore}", True, "black")
 
-coordinates = [0, 50, 100, 150, 200, 250, 300, 350, 400, 450]
+coordinates = []
+for i in range(0, 480, 20):
+    coordinates.append(i)
 
-rect_width = 50
-rect_height = 50
+rect_width = 20
+rect_height = 20
 rect_color = (255, 0, 0)  # Red color represented by RGB values
 rect_x = random.choice(coordinates)
 rect_y = random.choice(coordinates)
-x_cor = 250
-y_cor = 250
-space = 50
+x_cor = 240
+y_cor = 240
+space = 20
 
 while True:
 
@@ -49,14 +51,16 @@ while True:
     if keys[pygame.K_DOWN]:
         y_cor += space
 
+
     screen.fill([255,255,255])
+    
     screen.blit(Outer2_Surface,(0,0))
     screen.blit(Outer1_Surface,(0,0))
     screen.blit(game_surface,(75, 125))
     screen.blit(Font_surface,(10,10))
     screen.blit(Highscore_surface,(270, 10))
-    pygame.draw.rect(game_surface, rect_color, (rect_x, rect_y, rect_width, rect_height))
-    pygame.draw.rect(game_surface, (0, 0, 255), (x_cor, y_cor, rect_width, rect_height))
+    pygame.draw.rect(screen, rect_color, (rect_x + 75, rect_y + 125, rect_width, rect_height))
+    pygame.draw.rect(screen, (0, 0, 255), (x_cor + 75, y_cor + 125, rect_width, rect_height))
     pygame.display.update()
-    clock.tick(16)
+    clock.tick(7)
 
